50b0b8d0790e9389dbb290dab8ba7e056dc115e5
GalvanizeChris <galvanizechris@ChristophersMBP.PK5001Z>
2016-10-15 10:30:14 -0600

    first commit

---
+++ Program Challenges/4PC.js
@@ -0,0 +1,18 @@
+var scores = {
+	"John" : 3,
+	"Cindy" : 5 
+	"Tammy" : 2
+};
+
+scores["Tammy"] += 1;
+
+console.log(scores["Tammy"]);
+
+var myCrazyObject = {
+"name": "A ridiculous Object",
+"some array": [7,9, {purpose: "confusion", number :123}, 3.3],
+"random animal": "Banana Shark"
+};
+
+myCrazyObject["some array"][2].number
+123
---
+++ Program Challenges/6PC.js
@@ -0,0 +1,19 @@
+var name = "Chris";
+
+if (name === "Chris") {
+	console.log("Hey Chrissssy!")
+	}
+	
+	else {
+		console.log("Hi loser");
+	}
+	
+for (var i = 3; i < 10000; i = i * 3) {
+	console.log(i);
+	}
+	
+var animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"];
+
+for (var i = 0; i < animals.length; i++) {
+	console.log("Awesome " +animals[i] + "!")
+};
