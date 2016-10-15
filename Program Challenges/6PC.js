c3c0cb2ca690486381a8e46be5d837e766901090
GalvanizeChris <galvanizechris@ChristophersMBP.PK5001Z>
2016-10-15 11:41:33 -0600

    chapter 6 solutions

--- Program Challenges/6PC.js
+++ Program Challenges/6PC.js
@@ -17,3 +17,31 @@ var animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"];
 for (var i = 0; i < animals.length; i++) {
 	console.log("Awesome " +animals[i] + "!")
 };
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+var randomstring = "";
+
+console.log(randomstring.length);
+
+while (randomstring.length < 8) {
+	var x = Math.floor(Math.random() * alphabet.length);
+	randomstring += alphabet[x];
+}
+
+var input = "javascript is awesome";
+var output = "";
+
+for (var i = 0; i < input.length; i++) {
+	if (input[i] === "a") {
+		output += 4;
+	} else if (input[i] === "i") {
+		output += 1;
+	} else if (input[i] === "o") {
+		output += 0;
+	} else {
+	output += input[i];
+	}
+}
+
+console.log(output);
